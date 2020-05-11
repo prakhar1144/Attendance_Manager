09b90413825c9628159128011734cc7673e16c9d
Prakhar Pratyush <56781761+prakhar1144@users.noreply.github.com>
2020-05-11 23:35:35 +0530

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 2000 } );
-            $("#carousel-pause").click(function(){
-                $("#mycarousel").carousel('pause');
-            });
-            $("#carousel-play").click(function(){
-                $("#mycarousel").carousel('cycle');
-            });
-        });
-$(document).ready(function(){
-            $("#reserveBtn").click(function(){
-                $("#myModal").modal('show');
-            });
-        });
+   $("#loginBtn").click(function(){
+       $("#login").modal('show');
+   });
+});
 
 $(document).ready(function(){
-        $("#loginBtn").click(function(){
-            $("#login").modal('show');
-                });
-            });
+   setTimeout(function(){
+      $("#someidlikeloginabove").modal('show');
+   },1500);
+});
+
+$(document).ready(function(){
+   $("#registerBtn").click(function(){
+       $("#registeration").modal('show');
+   });
+});
+© 2020 GitHub, In
